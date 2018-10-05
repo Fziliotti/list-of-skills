7023cab407fce0e2ac1ddbe4d8130f547fa6c196
Fabricio Fernandes Ziliotti <ffz.ziliotti@gmail.com>
2018-10-05 01:01:28 -0300

    Primeiro commit do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>VueJs - List of skills</h1>
+    </header>
+
+    <nav>
+      <router-link to="/">Home</router-link>
+       <router-link to="/about">About</router-link>
+    </nav>
+
+    <transition name="router-anime">
+     <router-view/>
+    </transition>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Skills from "./components/Skills.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Skills
   }
-}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+  background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 80%;
+}
+nav {
+  padding: 20px 20px 20px 0;
+}
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+
+.router-anime-enter-active {
+  animation: coming .5s;
+  animation-delay: 0.5s;
+  opacity: 0;
+}
+
+.router-anime-leave-active {
+  animation: going .5s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+
+  to {
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+}
+
+@keyframes coming {
+  from {
+    transform: translateX(-50px);
+  }
+
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
 }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="about">
+        <h1>Hello {{their_name}} This is an about page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quidem deserunt eos commodi expedita unde sunt voluptatibus deleniti vel, ea ab laudantium necessitatibus quaerat accusantium beatae, qui, a aliquid aperiam!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Skills',
+        data(){
+            return {
+                their_name: this.$route.params.name
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
