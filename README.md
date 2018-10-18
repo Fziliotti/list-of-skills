ddb5edd77c6fd83ee3ca896e0c2669d739776e5c
Fabricio Ziliotti <ffz.ziliotti@gmail>
2018-10-18 15:11:52 -0300

    enviando

---
+++ README.md
@@ -0,0 +1,51 @@
+# list-of-skills -- website: https://fziliotti.github.io/list-of-skills/
+
+## Instalando dependencias
+```
+npm install
+```
+
+### Colocando em modo desenvolvimento
+```
+npm run serve
+```
+
+### Gerando a pasta dist (minificada)
+```
+npm run build
+```
+
+### Rodar testes
+```
+npm run test
+```
+
+### Lints e correções de arquivos
+```
+npm run lint
+```
+
+### Deploy -> Envio para githubPages
+#### Essa parte irá criar o dist e publicar essa pasta no branch gh:pages, para isso precisei configurar o vue.config.js e configurar o SSH
+##### https://cli.vuejs.org/guide/deployment.html#github-pages
+##### https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+```
+./deploy.sh
+```
+
+
+### O que estou aprendendo:
+- Vue CLI 3
+- Vue Router
+- Transitions em VueJs (com keyframes e com animate.css)
+- Templates VueJs
+- Babel
+- Fazer deploy do projeto VueJs
+
+
+### O que falta:
+- Configurar PWA (service worker) em VueJs
+- Aprender States e VUEX
+- Integrar Axios para fazer as requisições
+- Integrar algum banco de dados
+
---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Lista de skills</h1>
+    </header>
+
+    <nav>
+      <router-link to="/">Home</router-link>
+       <router-link to="/about">About</router-link>
+    </nav>
+
+    <transition name="router-anime">
+     <router-view/>
+    </transition>
+    
+  </div>
+</template>
+
+<script>
+import Skills from "./components/Skills.vue";
+export default {
+  name: "app",
+  components: {
+    Skills
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+  background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+  width: 80%;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+
+.router-anime-enter-active {
+  animation: coming .5s;
+  animation-delay: 0.5s;
+  opacity: 0;
+}
+
+.router-anime-leave-active {
+  animation: going .5s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+
+  to {
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+}
+
+@keyframes coming {
+  from {
+    transform: translateX(-50px);
+  }
+
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+</style>
